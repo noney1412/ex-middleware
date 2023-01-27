3d8b6cb7a2bd9b19d3d5a93c61a90f7db15a1484
nonpa <nonpanpila@gmail.com>
2023-01-27 12:08:55 +0700

    ✨ add types.

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+import type { AzureFunction, Context } from '@azure/functions';
+export declare function error(status: string, response: string, message?: string): void;
+/**
+ * @type TData is the type of the payload
+ * @type T1-7 are the bindings for the middleware functions
+ * */
+export type ExMiddleware<TPayload, T1 = any, T2 = any, T3 = any, T4 = any, T5 = any, T6 = any, T7 = any> = [Context, TPayload, T1, T2, T3, T4, T5, T6, T7];
+export type ExFlow<TMiddleware, TPayload> = {
+    middleware: TMiddleware;
+    payload: TPayload;
+};
+declare function exFlow<TFlow extends ExFlow<TFlow['middleware'], TFlow['payload']>>(middlewares: ((middleware: TFlow['middleware']) => TFlow['middleware'])[], initialPayload: TFlow['payload']): AzureFunction;
+export default exFlow;
